ddfece6051a52447aa43864bd4ca67acfe01827b
Manuel González Prigo <manuelgonzalezpriego1@gmail.com>
2024-11-29 16:22:53 +0100

    Update environment.prod.ts

--- sge/src/environments/environment.prod.ts
+++ sge/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true
 };
-export const URL_API = 'https://app.radfpd.es/api/private';
-export const URL_BASE = 'https://app.radfpd.es';
+export const URL_API = 'http://158.179.220.12/app.radfpd.es/api/private';
+export const URL_BASE = 'http://158.179.220.12/app.radfpd.es';
